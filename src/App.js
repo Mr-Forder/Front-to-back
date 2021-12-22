81511dcf0749476258fd32a264a3817bec8ef1df
Mr-Forder <colforder@gmail.com>
2021-12-22 08:03:22 +0000

    101 - conditonal rendering - 3 ways

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const comments = [
+    {
+      id: 0,
+      title: "1",
+      content: "Blah bloo blaah",
+    },
+    {
+      id: 1,
+      title: "2",
+      content: "flapstain",
+    },
+    {
+      id: 2,
+      title: "3",
+      content: "caroooo",
+    },
+    {
+      id: 3,
+      title: "4",
+      content: "Blah bleep blaah",
+    },
+    {
+      id: 4,
+      title: "5",
+      content: "Carpe diem",
+    },
+  ];
+
+  const loading = false;
+  const [showComments, setShowComments] = useState(false);
+
+  if (loading) return <h1>Loading...</h1>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Me list!</h1>
+      <button
+        onClick={() => {
+          setShowComments(!showComments);
+        }}
+      >
+        Show/hide comments
+      </button>
+      {showComments && (
+        <div className="comments">
+          <h2>{comments.length}</h2>
+          {comments.map((comment) => (
+            <div key={comment.id}>
+              <h3>{comment.title}</h3>
+              <p>{comment.content}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
