50a8e5c8211992c2726cbf350de72100f8c3b44a
Mr-Forder <colforder@gmail.com>
2021-12-22 08:12:24 +0000

    begin

--- src/App.js
+++ src/App.js
@@ -1,59 +1,32 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const comments = [
+  const list = [
     {
-      id: 0,
-      title: "1",
-      content: "Blah bloo blaah",
+      name: "1",
+      message: "Blah bloo blaah",
     },
     {
-      id: 1,
-      title: "2",
-      content: "flapstain",
+      name: "2",
+      message: "flapstain",
     },
     {
-      id: 2,
-      title: "3",
-      content: "caroooo",
+      name: "3",
+      message: "caroooo",
     },
     {
-      id: 3,
-      title: "4",
-      content: "Blah bleep blaah",
+      name: "4",
+      message: "Blah bleep blaah",
     },
     {
-      id: 4,
-      title: "5",
-      content: "Carpe diem",
+      name: "5",
+      message: "Carpe diem",
     },
   ];
 
-  const loading = false;
-  const [showComments, setShowComments] = useState(false);
-
-  if (loading) return <h1>Loading...</h1>;
   return (
     <div className="container">
-      <h1>Me list!</h1>
-      <button
-        onClick={() => {
-          setShowComments(!showComments);
-        }}
-      >
-        Show/hide comments
-      </button>
-      {showComments && (
-        <div className="comments">
-          <h2>{comments.length}</h2>
-          {comments.map((comment) => (
-            <div key={comment.id}>
-              <h3>{comment.title}</h3>
-              <p>{comment.content}</p>
-            </div>
-          ))}
-        </div>
-      )}
+      <h1>Hi there.</h1>
     </div>
   );
 };
